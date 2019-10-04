4c27b2c373e60b0af3cf96b7008be8e945a8c767
Ceara Owre <cearaowre@hotmail.com>
2019-10-03 23:15:03 -0400

    Adding some CSS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,33 @@
-// Random number to guess is between 19 and 120
-// Chrystal numbers are between 1 and 12
 
-// Here we establish the "targetNumber" (set to 50 in this example).
-//var targetNumber = 50;
-
-// Here we set the "number-to-guess" header to match the "targetNumber".
-// Eventually this will allow us to change the HTML to match the value in the JavaScript.
-//$("#number-to-guess").text(targetNumber);
+// Global Variables
 var currentScore = 0;
-//var counter = 0;
 var targetNumber =  0;
 var wins = 0;
 var losses = 0;
 
 // Here we created code that selects a number between 1 and 12.
 // We'll set this number at the start of the "game".
-//var targetNumberText = document.getElementById("number-to-guess");
 function setNumbers(){
   currentScore = 0;
-  $("#userScore-text").text("Your current score is: " + currentScore);
+  $("#userScore-text").text("Your current score: " + currentScore);
   $("#wins-text").text("Wins: " + wins);
   $("#losses-text").text("Losses: " + losses);
 
+  // Create random number to guess
   targetNumber = Math.floor(Math.random() * 120) + 19;
-  //var targetNumber = 5;
-  //console.log("Random NUmber to pass is: " + targetNumberText);
   console.log("Random NUmber to guess is: " + targetNumber);
+  // Add this number to page
   $("#number-to-guess").text(targetNumber);
 
-
-  //Create random values from 1 -12 for the cyrstal images
+  //Create random values from 1 -12 for the cyrstal images 
   var crystalValue = [];
   for (var i = 0; i < 4; i++) {
-    var numberOptions = [Math.round(Math.random() * 12)];
+    var numberOptions = [Math.floor(Math.random() * 12) + 1];
     console.log("NumberOptions is: " + numberOptions);
     crystalValue.push(Math.round(numberOptions));
     console.log("Crystal Value first: " + crystalValue);
   }
+  // Assign the random values to the crystal images
   $("#button1").attr("value", crystalValue[0]);
   $("#button2").attr("value", crystalValue[1]);
   $("#button3").attr("value", crystalValue[2]);
@@ -48,7 +39,7 @@ setNumbers();
 $(".crystal-image").on("click", function() {
   console.log("This button value is" + $(this).attr("value"));
   currentScore = currentScore + (Number($(this).attr("value")));
-  $("#userScore-text").text("Current Score: " + currentScore);
+  $("#userScore-text").text("Your current score: " + currentScore);
 
   if (currentScore > targetNumber) {
     console.log("You lose");
@@ -64,27 +55,3 @@ $(".crystal-image").on("click", function() {
 
 });
 
-
-// Create an on-click event that responds to button clicks of the crystal image.
-//$(".crystal-image").on("click", function() {
-
-    // Each time the user clicks the crystal the counter goes up by 1.
-  //  counter += 10;
-
-    //console.log("New Score: " + counter);
-
-    // Here we created some logic to "check" if the click counter matches the targetNumber.
-    // Remember, this click event will be triggered with each click.
-    // With each click the counter will increase by 10 and be re-evaluated against target.
-    //if (counter === targetNumber) {
-
-        // If the numbers match we'll celebrate the user's win.
-      //  console.log("You win!");
-   //   }
-   // else if (counter >= targetNumber) {
-
-        // Then they are alerted with a loss.
-        //console.log("You lose!!");
-    //}
-//console.log("random number is: " + increment);
-  //});
